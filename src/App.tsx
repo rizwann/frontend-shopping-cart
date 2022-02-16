026daa4cb1fe17c2bc98b052ecc80e4bfc95e103
Rizwan Kabir <rizwankabirsizan@gmail.com>
2022-02-17 00:29:53 +0100

    Dark Mode Toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import CartPage from "./pages/CartPage";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="*"
-          element={
-            <div
-              style={{ marginTop: "100px", textAlign: "center", color: "red" }}
-            >
-              {" "}
-              404 Not Found
-            </div>
-          }
-        />
+      <div className={darkMode ? "dark" : ""}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <div
+                style={{
+                  marginTop: "100px",
+                  textAlign: "center",
+                  color: "red",
+                }}
+              >
+                {" "}
+                404 Not Found
+              </div>
+            }
+          />
 
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
+          <Route
+            path="/cart"
+            element={
+              <CartPage darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            }
+          />
+        </Routes>
+      </div>
     </Router>
   );
 }
